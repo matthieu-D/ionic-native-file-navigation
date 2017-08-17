4fdeacdcc5deba46766e835051075b3550667490
Matthieu Drula <matthieu.drula.hs@gmail.com>
2017-08-16 21:21:55 -0400

    all working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+import { File } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items;
+  savedParentNativeURLs = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public fileNavigator: File, public plt: Platform) {
 
+    const ROOT_DIRECTORY = 'file:///';
+
+    plt.ready()
+      .then(() => {
+        this.listDir(ROOT_DIRECTORY, '');
+    })
   }
 
+  listDir = (path, dirName) => {
+    this.fileNavigator.listDir(path, dirName)
+      .then((entries) => {
+        this.items = entries;
+      })
+      .catch(this.handleError);
+  }
+
+  goDown = (item) => {
+    const parentNativeURL = item.nativeURL.replace(item.name, '');
+    this.savedParentNativeURLs.push(parentNativeURL);
+
+    this.listDir(parentNativeURL, item.name);
+  }
+
+  goUp = () => {
+    const parentNativeURL = this.savedParentNativeURLs.pop();
+
+    this.listDir(parentNativeURL, '');
+  }
+
+  handleError = (error) => {
+    console.log('error reading,', error)
+  };
 }
